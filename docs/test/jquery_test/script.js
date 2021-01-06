5977981640c82f5b18e482a7eaacf57d123ae65f
momizi06 <63577263+momizi06@users.noreply.github.com>
2021-01-06 11:56:54 +0900

    Update script.js
    いろいろ改造

--- docs/test/jquery_test/script.js
+++ docs/test/jquery_test/script.js
@@ -1,9 +1,35 @@
-$(function () {
-  var $body = $('body');
-  $('#js__sideMenuBtn').on('click', function () {
-    $body.toggleClass('side-open');
-    $('#js__overlay').on('click', function () {
-      $body.removeClass('side-open');
-    });
+(function($) {
+  var $nav   = $('#navArea');
+  var $btn   = $('.toggle_btn');
+  var $mask  = $('#mask');
+  var open   = 'open'; // class
+  // menu open close
+  $btn.on( 'click', function() {
+    if ( ! $nav.hasClass( open ) ) {
+      $nav.addClass( open );
+    } else {
+      $nav.removeClass( open );
+    }
+  });
+  // mask close
+  $mask.on('click', function() {
+    $nav.removeClass( open );
+  });
+} )(jQuery);
+
+
+$(function(){
+  $("#top_btn").on("click", function(){
+    $("html, body").animate({scrollTop: 0 }, 1000, "swing");
   });
 });
+
+
+/*==========
+作成中
+$(function () {
+    $btn.on('click', () => {
+        $("#top_btn").hide('slow');
+    });
+});
+==========*/
